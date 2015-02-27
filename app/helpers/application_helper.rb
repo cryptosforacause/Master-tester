8968c054d32425ecbaacdbd8c60960d87e448e1b
cryptosforacause <loopholemaster@gmail.com>
2015-02-27 19:38:46 +0530

    finishes chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns title based on presence of provided base title
+  def full_title(page_title='')
+    base_title="Ruby on Rails Tutorial and Expansion"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | {base_title}"
+    end
+  end
 end
